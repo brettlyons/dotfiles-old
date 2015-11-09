d094c2dc6eb034e0a88404bd38568ea953f27cf7
Brett Lyons <blyons@students.naropa.edu>
2015-11-09 09:12:42 -0700

    Added a lot of emacs config stuff

---
+++ .emacs.d/SessionDesktop.el
@@ -0,0 +1,33 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Sat Oct 31 18:52:36 2015
+;; Desktop file format version 206
+;; Emacs version 24.4.51.2
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 23) (left . 0) (unsplittable) (modeline . t) (width . 138) (height . 42) (fringe) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "Red") (background-mode . light) (horizontal-scroll-bars . t) (fullscreen) (alpha) (scroll-bar-width . 15) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 11) (left-fringe . 3) (line-spacing) (background-color . "White") (foreground-color . "Black") (vertical-scroll-bars . right) (internal-border-width . 0) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(138 42 ((0 0 142 20) (0 20 142 40)) (("~/Library/Application Support/Aquamacs Emacs/scratch buffer" "*scratch*" 1 1 ((tab "*scratch*" "/Users/brett/Library/Application Support/Aquamacs Emacs/scratch buffer"))) (nil "*Warnings*" 348 1 nil))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (22069 25172 121302 0) (desktop . "206") "brett@Macubus.lan" nil nil ((((font-backend mac-ct ns) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-12-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 0) (vertical-scroll-bars . right) (foreground-color . "Black") (background-color . "White") (line-spacing) (left-fringe . 3) (right-fringe . 11) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 15) (alpha) (fullscreen) (horizontal-scroll-bars . t) (display-type . color) (background-mode . light) (cursor-color . "Red") (visibility . t) (environment) (mouse-color . "black") (fringe) (frameset--id . "23E7-BA57-1FD6-805B") (frameset--mini t . t) (height . 42) (width . 138) (modeline . t) (minibuffer . t) (unsplittable) (left . 0) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "Macubus.lan") (explicit-name) (tool-bar-position . top)) ((min-height . 8) (min-width . 10) (min-height-ignore . 4) (min-width-ignore . 7) (min-height-safe . 2) (min-width-safe . 2) (min-pixel-height . 128) (min-pixel-width . 70) (min-pixel-height-ignore . 64) (min-pixel-width-ignore . 43) (min-pixel-height-safe . 32) (min-pixel-width-safe . 14)) vc (pixel-width . 996) (pixel-height . 662) (total-width . 142) (total-height . 41) (normal-height . 1.0) (normal-width . 1.0) (combination-limit) (leaf (pixel-width . 996) (pixel-height . 331) (total-width . 142) (total-height . 20) (normal-height . 0.5) (normal-width . 1.0) (buffer "*scratch*" (selected . t) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 1) (start . 1))) (leaf (last . t) (pixel-width . 996) (pixel-height . 331) (total-width . 142) (total-height . 20) (normal-height . 0.5) (normal-width . 1.0) (buffer "*Warnings*" (selected) (hscroll . 0) (fringes 3 11 nil) (margins nil) (scroll-bars 15 3 t nil) (vscroll . 0) (dedicated) (point . 348) (start . 1)))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history nil)
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 206
+  "/Users/brett/Library/Application Support/Aquamacs Emacs/scratch buffer"
+  "*scratch*"
+  'text-mode
+  '(visual-line-mode smart-spacing-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . utf-8) (truncate-lines)))
+
---
+++ .emacs.d/backups/!Users!brett!workspace!angular-unit1!questionanswers.md~
@@ -0,0 +1,14 @@
+** Why learn angular over other frameworks like Ember, Backbone, Knockout, etc?
+   * Because many companies are hiring for it, and/or have legacy software written in it.  And it's slightly more complicated and less performant than many other frameworks.  It's like running with a small parachute or a weightvest.
+
+** People have some very strong opinions about Angular. What are 3 common complaints people have about Angular?
+   * It's slow on mobile.
+   * It's complicated.
+   * it has a constant page-update lock that means every piece of the page is constantly being checked for needing to be updated
+   * there are many frameworks that either perform better, or facilitate faster development speed
+    
+** Is Angular an MVC framework?
+   * yes, a #client side# model-view-controller framework, or Model-View-Modelview framework.
+
+** Turn to the Angular docs. Find ng-app. What is it and what does it do? What does ng stand for?
+   * apprently NG stands for Core Module.  (WTF?)  according to the docs, ng-app is for auto-bootstrapping an application, and it designates the root element of a document.
