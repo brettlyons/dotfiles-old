626f80ab2d39b54f8a995e0b53514e2568200c48
Brett Lyons <blyons@students.naropa.edu>
2015-11-29 14:30:22 -0700

    Modified emacs init.el for OS differentials

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,18 +14,43 @@
 ;;(add-hook 'js-mode-hook
 ;;    (function (lambda ()
 ;;        (setq evil-shift-width js-indent-level))))
-; adds line numbers to sourcecode files
+
+;; adds line numbers to sourcecode files
 (add-hook 'prog-mode-hook
   (function (lambda ()
-      (linum-mode 1))))
+    (linum-mode 1))))
+
+
+(setq-default c-basic-offset 2
+              tab-width 2
+              indent-tabs-mode nil)
+
+;; (defun my-java-conf ()
+;;   "Java mode mods"
+;;   (setq indent-tabs-mode nil)
+;;   (setq indent-level 2)
+;;   (setq tab-width 2))
+
+; java indent should be 2
+;; (add-hook 'java-mode-hook 'my-java-conf)
 
 ;;(let ((default-directory "/usr/share/emacs/site-lisp/"))
 ;;  (normal-top-level-add-subdirs-to-load-path))
 
 ;;(setq x-select-enable-clipboard-manager nil)
 
-(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
-   (normal-top-level-add-subdirs-to-load-path))
+;;(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
+;;  (normal-top-level-add-subdirs-to-load-path))
+
+
+;; different site-lisp dirs for different OS's -- enabling shared home
+(cond 
+  ((string-equal system-type "gnu/linux")
+    (let ((default-directory "/usr/share/emacs/site-lisp/"))
+      (normal-top-level-add-subdirs-to-load-path)))
+  ((string-equal system-type "darwin")
+    (let (default-directory "/usr/local/share/emacs/site-lisp/")
+      (normal-top-level-add-subdirs-to-load-path))))
 
 ;; autosave to file instead of #filename#
 ;; (defun save-buffer-if-visiting-file (&optional args)
@@ -82,8 +107,12 @@
 (define-key evil-normal-state-map (kbd "SPC") 'ace-jump-mode)
 
 ;; start fullscreen
-(custom-set-variables
- '(initial-frame-alist (quote ((fullscreen . maximized)))))
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;;(toggle-frame-maximized)
+;; (custom-set-variables
+;;  '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
 
 ; auto-complete mode
 (global-auto-complete-mode 1)
@@ -96,6 +125,7 @@
 (unless (display-graphic-p)
   (require 'evil-terminal-cursor-changer))
 
+;; I like to have a box when naving and a bar when inserting
 (setq evil-normal-state-cursor '("green" box))
 (setq evil-insert-state-cursor '("green" bar))
 
@@ -106,6 +136,14 @@
 ;; (define-key evil-normal-state-map "gT" 'elscreen-previous) ;previous tab
 ;; (define-key evil-normal-state-map "gt" 'elscreen-next) ;next tab
 
+;; Java stuff, with JDEE
+;;(add-to-list 'load-path (format "%s/dist/jdee-2.4.1/lisp" my-jdee-path))
+;;  (autoload 'jde-mode "jde" "JDE mode" t)
+;;  (setq auto-mode-alist
+;;        (append '(("\\.java\\'" . jde-mode)) auto-mode-alist))
+(add-hook 'java-mode-hook (function (lambda () 
+                            (require 'gradle-mode)
+                            (gradle-mode 1))))
 
 ;; eslint, jsx stuff
 ;; use web-mode for .jsx files
@@ -174,10 +212,10 @@
  '(cua-normal-cursor-color "#839496")
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
- '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-enabled-themes (quote (calmer-forest)))
  '(custom-safe-themes
    (quote
-    ("790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "7997e0765add4bfcdecb5ac3ee7f64bbb03018fb1ac5597c64ccca8c88b1262f" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
+    ("f5eb916f6bd4e743206913e6f28051249de8ccfd070eae47b5bde31ee813d55f" "790e74b900c074ac8f64fa0b610ad05bcfece9be44e8f5340d2d94c1e47538de" "7997e0765add4bfcdecb5ac3ee7f64bbb03018fb1ac5597c64ccca8c88b1262f" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "11636897679ca534f0dec6f5e3cb12f28bf217a527755f6b9e744bd240ed47e1" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
  '(fci-rule-color "#073642")
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
  '(highlight-symbol-colors
@@ -243,12 +281,14 @@
  '(xterm-color-names-bright
    ["#002b36" "#cb4b16" "#586e75" "#657b83" "#839496" "#6c71c4" "#93a1a1" "#fdf6e3"]))
 
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 160 :family "Anonymous Pro")))))
+ '(default ((t (:height 110 :family "Anonymous Pro")))))
+
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
