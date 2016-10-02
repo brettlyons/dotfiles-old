b33d77d1a9ab0d9cd841440748e49b79e58f5f99
Brett Lyons <blyons@students.naropa.edu>
2016-10-02 10:36:55 -0600

    makelinks.sh now links .vim to .config/nvim

--- makelinks.sh
+++ makelinks.sh
@@ -2,4 +2,4 @@
 ln -s ~/dotfiles/.i3/config ~/.config/i3/config
 ln -s ~/dotfiles/.Xresources ~/.Xresources
 ln -s ~/dotfiles/.emacs.d/init.el ~/.emacs.d/init.el
-
+ln -s ~/dotfiles/.vim ~/.config/nvim
