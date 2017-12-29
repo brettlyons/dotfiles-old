c4365586eb9ecde847e8a7f148715d69e9f038d2
Brett Lyons <blyons@students.naropa.edu>
2017-12-29 12:49:36 -0700

    Commented out some unnecessary parts of cpdotfiles.sh

--- cpdotfiles.sh
+++ cpdotfiles.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-cp ~/dotfiles/.i3/config ~/.config/i3/config
-cp ~/dotfiles/.Xresources ~/.Xresources
+# Don't need as many of these as I used to.
+# cp ~/dotfiles/.i3/config ~/.config/i3/config
+# cp ~/dotfiles/.Xresources ~/.Xresources
 cp ~/dotfiles/.spacemacs ~/
 cp ~/dotfiles/.zshrc ~/.zshrc
 cp ~/dotfiles/.zshenv ~/.zshenv
-cp ~/dotfiles/.vimrc ~/.vimrc
-cp -r ~/dotfiles/.vim ~/.config/nvim
-ln -s ~/.vim ~/.config/nvim
-cp -r ~/dotfiles/.vim ~/.vim
+# cp ~/dotfiles/.vimrc ~/.vimrc
+# cp -r ~/dotfiles/.vim ~/.config/nvim
+# ln -s ~/.vim ~/.config/nvim
+# cp -r ~/dotfiles/.vim ~/.vim
 # install spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 # and antigen
